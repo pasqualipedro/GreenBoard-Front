8f1793babc4508124927d6cb246788535254dd0a
pasqualipedro <pasqualipedro@gmail.com>
2021-10-19 00:26:32 +0100

    Loading feature Done - may need some improvements

---
+++ src/Components/NavbarPublic/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+
+class NavbarPublic extends Component {
+
+render() {
+    return (
+        <>
+        <h1>Thats the NavbarPublic</h1>
+        </>
+    )
+}
+
+};
+
+export default NavbarPublic;
--- src/Components/TransactionModal/index.js
+++ src/Components/TransactionModal/index.js
@@ -1,89 +1,12 @@
 import React from "react";
 import { Component } from "react";
-import styled from "styled-components";
+import Test from "./indexTest";
 
-const Wrapper = styled.div`
-  .btn-modal {
-    padding: 10px 20px;
-    display: block;
-    margin: 100px auto 0;
-    font-size: 18px;
-  }
-
-  .modalPlaylist,
-  .overlayLayer {
-    width: 100vw;
-    height: 100vh;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    position: fixed;
-  }
-
-  .overlayLayer {
-    background: rgba(49, 49, 49, 0.8);
-  }
-  .modalPlaylist-content {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    line-height: 1.4;
-    background: #f1f1f1;
-    padding: 14px 28px;
-    border-radius: 3px;
-    max-width: 600px;
-    min-width: 200px;
-    max-height: 500px;
-    overflow-y: auto;
-    overflow-x: hidden;
-    color: rgb(23, 32, 42);
-  }
-
-  .close-modal {
-    position: absolute;
-    top: 10px;
-    right: 10px;
-    padding: 5px 7px;
-  }
-
-  .playlistCover {
-    width: 200px;
-    height: 200px;
-    border-radius: 10px;
-  }
-
-  .trackList {
-    text-decoration: none;
-    color: rgb(23, 32, 42);
-  }
-
-  .coverPlayButton {
-    padding: 0;
-    margin: 15px;
-    display: flex;
-    transition: 400ms;
-    cursor: pointer;
-
-    &:hover {
-      transform: scale(1.025);
-    }
-  }
-`;
 
 class TransactionModal extends Component {
   render() {
     return (
-      <Wrapper>
-        {this.props.open && this.props.playlistInfo.id && (
-          //-MODAL HERE
-          <div className="modalPlaylist">
-            <div
-              className="overlayLayer"
-              onClick={this.props.togglePlaylist}
-            ></div>
-            <div className="modalPlaylist-content">
+      <Test open={ this.props.open && this.props.playlistInfo.id } >
               <h1>I´m feeling like... {this.props.mood}</h1>
               <div className="alignInY">
                 <a
@@ -117,10 +40,7 @@ class TransactionModal extends Component {
                   </a>
                 ))}
               </ul>
-            </div>
-          </div>
-        )}
-      </Wrapper>
+      </Test>
     );
   }
 }
